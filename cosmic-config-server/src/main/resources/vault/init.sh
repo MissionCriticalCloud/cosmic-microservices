5687a08bc6e9c0f5862943e2890437c9669d2e39
Elias Gomes <egomes@schubergphilis.com>
2017-07-28 16:16:07 +0200

    Added Bill Viewer development configuration

--- cosmic-config-server/src/main/resources/vault/init.sh
+++ cosmic-config-server/src/main/resources/vault/init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+vault write secret/cosmic-bill-viewer @/missioncriticalcloud/cosmic-bill-viewer.json
 vault write secret/cosmic-metrics-collector @/missioncriticalcloud/cosmic-metrics-collector.json
 vault write secret/cosmic-usage-api @/missioncriticalcloud/cosmic-usage-api.json
